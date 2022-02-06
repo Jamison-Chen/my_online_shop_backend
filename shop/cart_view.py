d38a871777ea6f1802211deb0c1b9da81cb5f524
Jamison Chen <jamisonforeverandever@gmail.com>
2022-02-06 16:14:04 +0800

    update

--- shop/cart_view.py
+++ shop/cart_view.py
@@ -18,35 +18,46 @@ def index(request):
         if operation == "create":
             cstmr = customer.objects.get(token=token)
             try:
-                crt = cart.objects.get(customer=cstmr)
+                crt = cstmr.cart
             except:
                 crt = cart.objects.create(customer=cstmr)
             inv = inventory.objects.get(id=request.POST.get("inventory_id", default=""))
-            quantity = int(request.POST.get("quantity", default=""))
-
-            # TODOS: check if the inventory quantity is sufficient.
-            # If yes, update the inventory quantity;
-            # if no, report "failed" status to the front end.
+            quantity = abs(int(request.POST.get("quantity", default="")))
             subTotal = inv.product.unit_price * quantity
             citm = crt.items.filter(inventory=inv)
+            invIsSufficient = True
             if citm:
-                citm.update(
-                    quantity=citm[0].quantity + quantity,
-                    subtotal_costs=citm[0].subtotal_costs + subTotal,
-                )
+                newQuantity = citm[0].quantity + quantity
+                if inv.inventory >= newQuantity:
+                    citm.update(
+                        quantity=newQuantity,
+                        subtotal_costs=citm[0].subtotal_costs + subTotal,
+                    )
+                else:
+                    invIsSufficient = False
             else:
-                citm = cart_item.objects.create(
-                    cart=crt, inventory=inv, quantity=quantity, subtotal_costs=subTotal
-                )
-
-            # re-fetch the cart due to the change of its many-to-many field
-            crt = cart.objects.filter(customer=cstmr)
-            newTotalCosts = crt[0].items.all().aggregate(t=Sum("subtotal_costs"))["t"]
-            crt.update(
-                total_costs=newTotalCosts, freight=60 if newTotalCosts < 100 else 0
-            )
-            res["data"] = {"total_costs": crt[0].total_costs, "freight": crt[0].freight}
-            res["status"] = "succeeded"
+                if inv.inventory >= quantity:
+                    citm = cart_item.objects.create(
+                        cart=crt,
+                        inventory=inv,
+                        quantity=quantity,
+                        subtotal_costs=subTotal,
+                    )
+                else:
+                    invIsSufficient = False
+            if invIsSufficient:
+                newTotalCosts = crt.items.all().aggregate(t=Sum("subtotal_costs"))["t"]
+                crt.total_costs = newTotalCosts
+                crt.freight = 60 if newTotalCosts < 100 else 0
+                crt.save()
+                res["data"] = {"total_costs": crt.total_costs, "freight": crt.freight}
+                res["status"] = "succeeded"
+            else:
+                res["data"] = {
+                    "total_costs": crt.total_costs,
+                    "freight": crt.freight,
+                }
+                res["status"] = "inventory not sufficient"
         elif operation == "read":
             res["data"] = {"total_costs": 0, "cart_items": []}
             crt = cart.objects.filter(customer__token=token)
@@ -54,31 +65,27 @@ def index(request):
                 crt = crt[0]
                 res["data"]["total_costs"] = crt.total_costs
                 res["data"]["freight"] = crt.freight
-                for eachCartItem in crt.items.all():
+                for eachItem in crt.items.all():
                     res["data"]["cart_items"].append(
                         {
-                            "cart_item_id": eachCartItem.id,
-                            "product_id": eachCartItem.inventory.product.id,
-                            "product_name": eachCartItem.inventory.product.name,
-                            "color": eachCartItem.inventory.color.detail,
-                            "size": eachCartItem.inventory.size.detail,
-                            "unit_price": eachCartItem.inventory.product.unit_price,
-                            "quantity": eachCartItem.quantity,
-                            "subtotal_costs": eachCartItem.subtotal_costs,
+                            "cart_item_id": eachItem.id,
+                            "product_id": eachItem.inventory.product.id,
+                            "product_name": eachItem.inventory.product.name,
+                            "color": eachItem.inventory.color.detail,
+                            "size": eachItem.inventory.size.detail,
+                            "unit_price": eachItem.inventory.product.unit_price,
+                            "quantity": eachItem.quantity,
+                            "subtotal_costs": eachItem.subtotal_costs,
                         }
                     )
         elif operation == "delete":
-            cstmr = customer.objects.get(token=token)
-            crt = cart.objects.get(customer=cstmr)
+            crt = customer.objects.get(token=token).cart
             citm = crt.items.get(id=request.POST.get("cart_item_id")).delete()
-
-            # re-fetch the cart due to the change of its many-to-many field
-            crt = cart.objects.filter(customer=cstmr)
-            newTotalCosts = crt[0].items.all().aggregate(t=Sum("subtotal_costs"))["t"]
-            crt.update(
-                total_costs=newTotalCosts, freight=60 if newTotalCosts < 100 else 0
-            )
-            res["data"] = {"total_costs": crt[0].total_costs, "freight": crt[0].freight}
+            newTotalCosts = crt.items.all().aggregate(t=Sum("subtotal_costs"))["t"]
+            crt.total_costs = newTotalCosts
+            crt.freight = 60 if newTotalCosts < 100 else 0
+            crt.save()
+            res["data"] = {"total_costs": crt.total_costs, "freight": crt.freight}
             res["status"] = "succeeded"
         else:
             return HttpResponseNotFound()
